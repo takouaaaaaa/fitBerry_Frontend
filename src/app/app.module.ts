9b553139ee2dc7460f091fd395e80a7d36f7259c
ineshajri123789 <ineshajri20311254@gmail.com>
2025-10-21 11:09:53 +0100

    update-front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
